f22fa42951432de83e479a499a3117411f2422a3
Simon Cropp <simon.cropp@gmail.com>
2026-07-08 22:15:03 +1000

    cleanup

---
+++ src/SponsorCheck/OverrideableCodes.cs
@@ -0,0 +1,16 @@
+﻿public static class OverrideableCodes
+{
+    // Single source of truth for the codes the OSS author can tune at pack time. The bundler,
+    // verifier, targets file, and docs all derive their metadata names from this list. A new
+    // entry needs accompanying bundler properties + targets-file plumbing.
+    public static readonly OverrideableCode[] All =
+    [
+        new("SC001", "SC002", "SC021", "NoLicenseSpecified"),
+        new("SC005", "SC006", "SC023", "LicenseIgnored"),
+        new("SC007", "SC008", "SC024", "InvalidAccount"),
+        new("SC009", "SC010", "SC025", "LicenseExpired"),
+    ];
+
+    public static HashSet<string> Codes { get; } =
+        [with(All.SelectMany(_ => new[] {_.Code, _.CpmCode, _.OwnerCode}), StringComparer.Ordinal)];
+}
